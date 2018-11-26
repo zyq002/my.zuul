4989a260312b99b56cbed485fad8481f1eebbc22
zyq002 <87518156@qq.com>
2018-11-26 18:58:55 +0800

    9

--- src/main/java/com/example/demo/ZuulApplication.java
+++ src/main/java/com/example/demo/ZuulApplication.java
@@ -2,25 +2,28 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-
-import com.common.filter.AccessFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableDiscoveryClient
 @EnableHystrix
-public class ZuulApplication {
+public class ZuulApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
 	}
-	
-//	@Bean
-//	public AccessFilter accessFilter() {
-//		return new AccessFilter();
-//	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		builder.sources(this.getClass());
+		return super.configure(builder);
+	}
+
+	// @Bean
+	// public AccessFilter accessFilter() {
+	// return new AccessFilter();
+	// }
 }
